e9a50c01bd273bb1258a9b1143368b0880bb066c
Aurora Jitrskul <aurawra@outlook.com>
2025-10-08 18:58:14 -0500

    moved path planning code to an external directory and imported it into the godot project successfully as an outside library

--- reeds-shepp_2d/scripts/UI/PathPlanningLib/Geometry/MathUtils.cs
+++
@@ -1,1 +0,0 @@
-namespace PathPlanningLib.Geometry;
--- reeds-shepp_2d/scripts/UI/PathPlanningLib/PathPlanners/IPathPlanner.cs
+++
@@ -1,9 +0,0 @@
-namespace PathPlanningLib.PathPlanners;
-using PathPlanningLib.Geometry;
-public interface IPathPlanner<TKinematics>
-{
-    Path PlanPath(
-        Pose start,
-        Pose goal,
-        TKinematics model);
-}
