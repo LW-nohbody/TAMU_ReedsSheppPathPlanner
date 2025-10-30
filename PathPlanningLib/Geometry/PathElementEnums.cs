3945d370df7b5b622d7e4b99187016013c7f3b1d
Aurora Jitrskul <aurawra@outlook.com>
2025-10-30 16:52:00 -0500

    Deleted pathplanninglib changes from digsim3d

--- PathPlanningLib/Geometry/PathElementEnums.cs
+++
@@ -1,7 +0,0 @@
-namespace PathPlanningLib.Algorithms.Geometry.PathElements;
-
-// Represents Reeds-Shepp/Dubins steering commands
-public enum Steering { LEFT = -1, RIGHT = 1, STRAIGHT = 0 }
-
-// Represent forwards/backwards motion
-public enum Gear { FORWARD = 1, BACKWARD = -1 }
