0c0aa68ed6c077059fe3a039d4a2fa2a81c6c579
LW-nohbody <lance.wiley09@tamu.edu>
2025-11-22 11:49:35 -0600

    Documented and filled out README

--- DigSim3D/Scripts/Services/Interfaces/IPathPlanner.cs
+++ DigSim3D/Scripts/Services/Interfaces/IPathPlanner.cs
@@ -1,6 +1,9 @@
 using DigSim3D.Domain;
 namespace DigSim3D.Services;
 
+/// <summary>
+/// Interface for the HybridPathPlanners, allows for polymorphism with vehicles
+/// </summary>
 public interface IPathPlanner
 {
   PlannedPath Plan(Pose start, Pose goal, VehicleSpec spec, WorldState world);
