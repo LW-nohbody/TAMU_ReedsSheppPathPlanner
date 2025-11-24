649b56e4736df1199b72ac66d1169c4db2578415
LW-nohbody <lance.wiley09@tamu.edu>
2025-11-24 14:09:12 -0600

    Completed comments, and started README

--- DigSim3D/Scripts/Debug/DebugPath.cs
+++ DigSim3D/Scripts/Debug/DebugPath.cs
@@ -5,11 +5,21 @@ using System.Text;
 
 namespace DigSim3D.Debugging
 {
+    /// <summary>
+    /// Prints debugging information for path generation
+    /// </summary>
     public static class DebugPath
     {
         public static bool Enabled = true;
         public static int  Verbosity = 2;
 
+        /// <summary>
+        /// Prints begin of path for vehicle
+        /// </summary>
+        /// <param name="who"></param>
+        /// <param name="vehId"></param>
+        /// <param name="sliceId"></param>
+        /// <returns></returns>
         public static string Begin(string who, int vehId, int sliceId)
         {
             string id = $"{who}/veh{vehId}/slice{sliceId}/{DateTime.UtcNow.Ticks}";
@@ -17,6 +27,12 @@ namespace DigSim3D.Debugging
             return id;
         }
 
+        /// <summary>
+        /// Prints params of path
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="tag"></param>
+        /// <param name="kv"></param>
         public static void Check(string id, string tag, params (string k, object v)[] kv)
         {
             if (!Enabled || Verbosity < 2) return;
@@ -25,6 +41,12 @@ namespace DigSim3D.Debugging
             // GD.Print($"[PATH/CHECK] {id} :: {tag}:{sb}");
         }
 
+        /// <summary>
+        /// Prints end of path
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="status"></param>
+        /// <param name="kv"></param>
         public static void End(string id, string status="ok", params (string k, object v)[] kv)
         {
             if (!Enabled || Verbosity < 1) return;
@@ -33,6 +55,11 @@ namespace DigSim3D.Debugging
             GD.Print($"[PATH/END] {id} :: {status}{sb}");
         }
 
+        /// <summary>
+        /// Helper function to parse params
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
         static string Fmt(object v) => v switch
         {
             float f  => f.ToString("0.000000", CultureInfo.InvariantCulture),
---
+++ DigSim3D/Scripts/README.md
@@ -0,0 +1,5 @@
+
+
+
++ [SimulationConfig](Config/SimulationConfig.cs) - Configures the simulation details, ie: robot speed, dig depth, etc
++ [DebugPath](Debug/DebugPath.cs) - Prints debugging information for path generation
